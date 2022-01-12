79a5301755bca0991d108785aa250d64cc552844
David Ackerly <dackerly@berkeley.edu>
2022-01-11 22:42:10 -0800

    pivovaroff data added; analysis script updated; sample table output and converted to Excel.

--- scripts/LFM_analysis.R
+++ scripts/LFM_analysis.R
@@ -5,8 +5,27 @@ library(ggplot2)
 lw <- read.csv('data/PWD_Oct2021_LFM_WP_Calcs.csv',as.is=T)
 str(lw)
 
+# for analysis, remove one sample of Baccharis that had no LFM
+lw <- lw[-which(is.na(lw$Bulk.LFM)),]
+
+# sample sizes
+table(lw$Species)
+write.csv(table(lw$Species,lw$Site),'results/sampletable.csv')
+table(lw$Species,lw$Site)
+nrow(lw)
+
 # Quick look!
 plot(lw$Midday.mean,lw$Bulk.LFM)
+
+## compare to Pivovaroff study
+p <- read.csv('data/other_studies/Pivovaroff_WP_vs_LFM.csv',as.is=T)
+head(p)
+p$bulkLFM <- p$LFM_./100
+
+par(mar=c(5,5,1,1))
+plot(p$WP_md_MPa,p$bulkLFM,pch=19, xlab='Midday water potential (MPa)',ylab='Bulk live fuel moisture',cex.lab=2)
+points(lw$Midday.mean,lw$Leaf.LFM,pch=19,col='red',cex=2)
+legend(-7,2.5,legend = c('Pivovaroff','this study'),fill = c('black','red'),cex = 2)
 
 # plotting inverse of LFM makes it fairly linear
 lw$iBulk.LFM <- 1/lw$Bulk.LFM
@@ -19,22 +38,28 @@ gploti <- ggplot(lw) +
   geom_abline(slope=ifit$coefficients[2],intercept=ifit$coefficients[1]) + 
   labs(x='Midday mean water potential (MPa)',y='Inverse of live fuel moisture') 
 
-print(gplot)
+print(gploti)
 print(gploti) + geom_hline(yintercept=1/0.7)
 gplot + facet_wrap(~Species,ncol=4)
 
 # not inverse
-fit <- lm(Bulk.LFM~Midday.mean,data=lw)
+xbp <- which(lw$Species=='BACPIL')
 
-gplot <- ggplot(lw) + 
-  geom_point(aes(x=Midday.mean, y=Bulk.LFM,color=Species)) +
+#choose whether to use all points or without bacpil
+xx <- lw
+xx <- lw[-xbp,]
+
+fit <- lm(Bulk.LFM~Midday.mean,data=xx)
+
+gplot <- ggplot(xx) + 
+  geom_point(aes(x=Midday.mean, y=Bulk.LFM,color=Species),size=4) +
   geom_abline(slope=fit$coefficients[2],intercept=fit$coefficients[1]) + 
   labs(x='Midday mean water potential (MPa)',y='Live fuel moisture (%)') 
 
 print(gplot)
 print(gplot) + geom_hline(yintercept=0.7)
 
-gplot + geom_hline(yintercept=0.7) + facet_wrap(~Species,ncol=4)
+gplot + facet_wrap(~Species,ncol=4)
 
 objplot(iBulk.LFM~Midday.mean,data=lw,pch=19)
 abline(fit)
@@ -55,16 +80,19 @@ rownames(spMeans)
 spMeans$sp2 <- c('Cc','Af','Qr','Qd','Cp','Qg','Qa','Qk','Ha','Uc','Am','Pm','Bp')
 spMeans$sp6 <- c('Ceacun','Adefas','Quedur','Quedou','Ceapar','Quegar','Queagr','Quekel','Hetarb','Umbcal','Arbmen','Psemen','Bacpil')
 
-op=par(mfrow=c(2,3))
+op=par(mfrow=c(2,3),mar=c(4,4,2,1))
 barplot(spMeans$BulkLFM,main='Bulk LFM',names.arg = spMeans$sp2)
 barplot(spMeans$LeafLFM,main='Leaf LFM',names.arg = spMeans$sp2)
 barplot(spMeans$StemLFM,main='Stem LFM',names.arg = spMeans$sp2)
-barplot(spMeans$MiddayWP,main='Midday WP',names.arg = spMeans$sp2)
-barplot(spMeans$PredawnWP,main='Predawn WP',names.arg = spMeans$sp2)
+barplot(spMeans$MiddayWP,main='Midday WP',names.arg = spMeans$sp2, ylim=c(-6.5,0))
+barplot(spMeans$PredawnWP,main='Predawn WP',names.arg = spMeans$sp2, ylim=c(-6.5,0))
 barplot(spMeans$WPdiff,main='WP differential',names.arg = spMeans$sp2)
 par(op)
-
 
 pairs(spMeans)
 plot(spMeans$MiddayWP,spMeans$BulkLFM,type='n')
 text(spMeans$MiddayWP,spMeans$BulkLFM,labels=spMeans$sp6)
+
+
+
+
