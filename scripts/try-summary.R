be3a5f8a3eedbc9c05bd563b11620cccb402fbdd
bovingigit config --global user.name Jane <indraboving@gmail.com>
2026-01-23 11:23:46 -0800

    Found more traits; updated correlations with new traits, organized into 3 separate Rmds for analysis.

--- scripts/try-summary.R
+++ scripts/try-summary.R
@@ -1,3 +1,61 @@
+
+#####Start here #####
+
+#Data in  "species-traits-withseki.csv". Compiled in Excel. 
+traits_compiled_df <- read_csv(here("data", "species-traits-withseki.csv")) %>% 
+  clean_names() %>% 
+  mutate(species = sp_code)
+
+
+#Add rooting depth info from here:https://www.groundwaterresourcehub.org/where-we-work/california/plant-rooting-depth-database/
+rd_df <- read_csv(here("data", "other_studies", "Plant_Rooting_Depth_Database_20210525_update_sheet2.csv")) %>% 
+  clean_names() %>% 
+  select(scientific_name, max_rooting_depth_m) %>%
+  mutate(Genus_species = scientific_name) %>% 
+  merge(., SpCodes,  by = c("Genus_species")) %>% 
+  select(SpCode6, 3, 4) %>% 
+  mutate(min_or_max = case_when(
+    max_rooting_depth_m %in% c("> 2.45") ~ "min",
+    max_rooting_depth_m %in% c("> 2.74") ~ "min",
+    max_rooting_depth_m %in% c("> 1.52") ~ "min",
+    max_rooting_depth_m %in% c("> 1.22") ~ "min",
+    max_rooting_depth_m %in% c(">4") ~ "min",
+    max_rooting_depth_m %in% c("> 0.61") ~ "min",
+    TRUE ~ as.character("max")
+  )) %>% 
+  mutate(max_rooting_depth_m = case_when(
+    max_rooting_depth_m %in% c("> 2.45") ~ 2.45,
+    max_rooting_depth_m %in% c("> 2.74") ~ 2.74,
+    max_rooting_depth_m %in% c("> 1.52") ~ 1.52,
+    max_rooting_depth_m %in% c("> 1.22") ~ 1.22,
+    max_rooting_depth_m %in% c(">4") ~ 4,
+    max_rooting_depth_m %in% c("> 0.61") ~ 0.61,
+    TRUE ~ as.numeric(max_rooting_depth_m)
+  )) %>% 
+  rename(sp_code = SpCode6) %>% 
+  group_by(sp_code) %>% 
+  mutate(max_rooting_depth_m = max(max_rooting_depth_m, na.rm = T)) %>% 
+  mutate(max_rooting_depth_m = case_when(
+    max_rooting_depth_m %in% c(-Inf) ~ NA, 
+    TRUE ~ as.numeric(max_rooting_depth_m)
+  )) %>% 
+  select(-min_or_max) %>% 
+  distinct() %>% 
+  ungroup()
+
+unique(rd_df$max_rooting_depth_m)
+
+traits_rd_df <- merge(rd_df, traits_compiled_df, by = c("sp_code"), all.y = T) %>% 
+  select(-dr_max_rooting_depth) 
+
+write_csv(traits_rd_df, here("data", "traits_rd_20250327.csv"))
+
+###---- Below all used for filling in master datasheet -----
+
+
+
+
+
 # summarize try trait data
 try <- read.csv(here('data', 'other_studies', 'try-trait-data.csv'))
 dim(try)
@@ -55,14 +113,13 @@ table(try$SpeciesName)
 table(try$OriglName)
 try$OrigValueStr[which(try$SpeciesName=='Quercus berberidifolia' & try$OriglName=='P50 (MPa)')]
 
-#------
+# OG code (David's)-----
 
 #This is the data we actually use, which is a combination of the following: 
 #1) David's trait data (here("data", "species-traits.csv)), not sure where it is made/written or written. Seems to be the data above? 
 
 SpCodes <- read_csv(here("data", "SpCodes.csv"))
 
-###-----------------
 # summarize try trait data for all species, 20241014
 try_all <- read.csv(here('data', 'try_all_20241014.csv')) %>% 
   select(SpeciesName, OriglName, StdValue) %>% 
@@ -73,7 +130,8 @@ try_all <- read.csv(here('data', 'try_all_20241014.csv')) %>%
   mutate(trait_name = case_when(
     OriglName %in% c("P50", "Water potential at 50% loss of conductivity Psi_50 (MPa)", "P50 (MPa)") ~ "P50",
     OriglName %in% c("P88", "P88 (MPa)") ~ "P88",
-    OriglName %in% c("SLA (cm2 g-1)", "SLA") ~ "SLA (cm2 g-1)",
+    OriglName %in% c("SLA (cm2 g-1)") ~ "SLA (cm2 g-1)",
+    OriglName %in% c("SLA") ~ "SLA",
     TRUE ~ OriglName
   )) %>% 
   select(-OriglName) %>% 
@@ -98,61 +156,10 @@ try_all <- read.csv(here('data', 'try_all_20241014.csv')) %>%
 #We used this to fill in what was missing in the species-traits-withseki.csv
 
 
-# 2) I (Indra) added in SEKI data and looked online to find other traits, which are now in the "species-traits-withseki.csv". Compiled in Excel. 
-traits_compiled_df <- read_csv(here("data", "species-traits-withseki.csv")) %>% 
-  clean_names() %>% 
-  mutate(species = sp_code)
 
 
-#Add rooting depth info from here:https://www.groundwaterresourcehub.org/where-we-work/california/plant-rooting-depth-database/
-rd_df <- read_csv(here("data", "other_studies", "Plant_Rooting_Depth_Database_20210525_update_sheet2.csv")) %>% 
-  clean_names() %>% 
-  select(scientific_name, max_rooting_depth_m) %>%
-  mutate(Genus_species = scientific_name) %>% 
-  merge(., SpCodes,  by = c("Genus_species")) %>% 
-  select(SpCode6, 3, 4) %>% 
-  mutate(min_or_max = case_when(
-    max_rooting_depth_m %in% c("> 2.45") ~ "min",
-    max_rooting_depth_m %in% c("> 2.74") ~ "min",
-    max_rooting_depth_m %in% c("> 1.52") ~ "min",
-    max_rooting_depth_m %in% c("> 1.22") ~ "min",
-    max_rooting_depth_m %in% c(">4") ~ "min",
-    max_rooting_depth_m %in% c("> 0.61") ~ "min",
-    TRUE ~ as.character("max")
-  )) %>% 
-  mutate(max_rooting_depth_m = case_when(
-    max_rooting_depth_m %in% c("> 2.45") ~ 2.45,
-    max_rooting_depth_m %in% c("> 2.74") ~ 2.74,
-    max_rooting_depth_m %in% c("> 1.52") ~ 1.52,
-    max_rooting_depth_m %in% c("> 1.22") ~ 1.22,
-    max_rooting_depth_m %in% c(">4") ~ 4,
-    max_rooting_depth_m %in% c("> 0.61") ~ 0.61,
-    TRUE ~ as.numeric(max_rooting_depth_m)
-  )) %>% 
-  rename(sp_code = SpCode6) %>% 
-  group_by(sp_code) %>% 
-  mutate(max_rooting_depth_m = max(max_rooting_depth_m, na.rm = T)) %>% 
-  mutate(max_rooting_depth_m = case_when(
-    max_rooting_depth_m %in% c(-Inf) ~ NA, 
-    TRUE ~ as.numeric(max_rooting_depth_m)
-  )) %>% 
-  select(-min_or_max) %>% 
-  distinct() %>% 
-  ungroup()
-  
-unique(rd_df$max_rooting_depth_m)
-  
-traits_rd_df <- merge(rd_df, traits_compiled_df, by = c("sp_code"), all.y = T) %>% 
-    select(-dr_max_rooting_depth) 
-                    
-write_csv(traits_rd_df, here("data", "traits_rd_20250327.csv"))
+#### Indra adding more in ----- 
 
-
-
-#### ----- 
-
-
-#Older data, not sure this is used?
 ###-----------------
 # summarize try trait data for SEKI
 tmn_seki <- read.csv(here('data', 'try_seki_traits_1.csv')) %>% 
@@ -192,5 +199,45 @@ tsd_seki <- read.csv(here('data', 'try_seki_traits_2.csv')) %>%
   mutate(tsd = sd(StdValue, na.rm = T)) %>% 
   select(-StdValue) %>% 
   distinct()
+
+#------
+
+#Fletcher et al, 2018 Ceanothus data: 
+
+cea_df <- read_csv(here('data', 'other_studies', "ceanothus_physiology_fletcher2018.csv"),
+                   skip = c(4)) %>% 
+  clean_names() %>%
+  select(1:14) %>% 
+  rename_with(~str_remove(., "_[0-9]+$")) %>% 
+  select(species, 
+         leaf_mass_per_area, 
+         leaf_dry_matter_content,
+         saturated_water_content, 
+         osmotic_potential_at_turgor_loss_point)%>%
+  mutate(across(-species, as.numeric)) %>% 
+  group_by(species) %>% 
+  summarise(lma = mean(leaf_mass_per_area, na.rm = T),
+            swa = mean(saturated_water_content, na.rm = T),
+            ldmc = mean(leaf_dry_matter_content, na.rm = T),
+            tlp  = mean(osmotic_potential_at_turgor_loss_point, na.rm = T),
+            sla = 1/lma * 10000
+            )
+
+#Boving et al., 2025 trait data
+
+seki_swc <- read_csv(here('data', 'other_studies', 'Bovingetal_2025_traits.csv')) %>% 
+  clean_names() %>% 
+  group_by(species) %>%
+  summarize(across(where(is.numeric), mean, na.rm = TRUE))
+  
+
+
+
+
+
+
+
+
+
 
 
